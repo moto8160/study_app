5378fbad12099fab2acfe11faad30ab664285cd9
mori <hokuto.8160@gmail.com>
2025-12-12 09:13:58 +0900

    20251212

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Next.js の URL
+    origin: true, // フロントURLだけにすべき
     credentials: true,
   });
 
